91defc2710375c9a1b9b5f78b22c79c63a16a10d
wjshin0407 <59620729+wjshin0407@users.noreply.github.com>
2020-01-07 17:29:51 -0500

    Add files via upload

---
+++ Capstone Assignment 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used mainly for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import Numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
